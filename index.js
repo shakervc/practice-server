508ebf41287b3dc2cab21672a616e931f5130f3f
schandrasekhar <shaker.chandrasekhar@blackboard.com>
2016-10-27 16:25:45 -0400

    all excpt POST initially working

--- index.js
+++ index.js
@@ -1,79 +1,151 @@
 // My Node/Express server serving student data
-//https://expressjs.com/en/starter/hello-world.html
-//I have initialized teh server to run from /users/schandrasekhar/WebstormProjects/practice-server1 as per instructions in above link
-//Some day I want to deploy the server to run in Heroku
-//I also want to read and wriet to database
-// Store this file as index.js in myapp. Run node index.js.
-//http://stackoverflow.com/questions/9177049/express-js-req-body-undefined
+
 // Having problems to get PUT to work
-//var bodyParser = require('body-parser');
+// I can POST hardcoded data. Probably PUT hardcoded data too. Problem is getting body data for PUT and POST. PUT and
+// POST work with the JSON server
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var physics_class =
-    [   { "fname": "Adi", "lname": "GaL","id": 1},
-        { "fname": "Marai", "lname": "Malai","id": 2},
-        { "fname": "Iru", "lname": "AANN","id": 3},
-        { "fname": "Oru", "lname": "PeNN","id": 4} ] ;
-var chemistry_class =
-    [   { "fname": "Matt", "lname": "Dixon","id": 1},
-        { "fname": "John", "lname": "Dean","id": 2},
-        { "fname": "Naanum", "lname": "Varen","id": 3},
-        { "fname": "Veda", "lname": "Nayagam","id": 4} ] ;
-var biology_class =
-    [   { "fname": "Raj", "lname": "Ars","id": 1},
-        { "fname": "Bur", "lname": "Fee","id": 2},
-        { "fname": "Naanum", "lname": "Varen","id": 3},
-        { "fname": "Veda", "lname": "Nayagam","id": 4} ] ;
+// Need to change POST code
+
+var courses = {
+    "physics":   [  { "fname": "Adi", "lname": "GaL","id": 1},
+                    { "fname": "Marai", "lname": "Malai","id": 2},
+                    { "fname": "Iru", "lname": "AANN","id": 3},
+                    { "fname": "Oru", "lname": "PeNN","id": 4} ],
+    "chemistry": [  { "fname": "Matt", "lname": "Dixon","id": 1},
+                    { "fname": "John", "lname": "Dean","id": 2},
+                    { "fname": "Naanum", "lname": "Varen","id": 3},
+                    { "fname": "Veda", "lname": "Nayagam","id": 4} ],
+    "biology":   [  { "fname": "Raj", "lname": "Ars","id": 1},
+                    { "fname": "Bur", "lname": "Fee","id": 2},
+                    { "fname": "Naanum", "lname": "Varen","id": 3},
+                    { "fname": "Veda", "lname": "Nayagam","id": 4} ]
+};
+
+var METHOD = {
+    PUT: 0,
+    DELETE: 1
+}
+function update(method, req, res) {
+
+    for (i = 0; i < courses[req.params.subject].length; i++) {
+
+        if (courses[req.params.subject][i].id == req.params.id) {
+
+            if (method == METHOD.DELETE) {
+                courses[req.params.subject].splice(i, 1);
+            } else if (method == METHOD.PUT) {
+                courses[req.params.subject][i].fname = req.body.fname;
+                courses[req.params.subject][i].lname = req.body.lname;
+            }
+            res.end(JSON.stringify(courses[req.params.subject]));
+
+        }
+
+    }
+}
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+// GET
 app.get('*', function (req, res, next) {
     res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
     next();
 });
+
+
 app.get('/physics', function (req, res) {
-    res.end (JSON.stringify(physics_class));
+    res.end (JSON.stringify(courses["physics"]));
 });
 app.get('/chemistry', function (req, res) {
-    res.end (JSON.stringify(chemistry_class));
+    res.end (JSON.stringify(courses["chemistry"]));
 });
 app.get('/biology', function (req, res) {
-    res.end (JSON.stringify(biology_class));
+    res.end (JSON.stringify(courses["biology"]));
 });
+
 app.get("*", function(request, response) {
     response.end("404!");
 });
-
+// OPTIONS
 app.options('*', function (req, res, next) {
     res.writeHead(200, {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "PUT" });
+        "Access-Control-Allow-Methods": "PUT, DELETE, POST" });
     next();
 });
 app.options('*', function (req, res) {
     res.end('Got a OPTIONS request at /physics/1');
 });
-
+// PUT
 app.put('/:subject/:id', function (req, res, next) {
     res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
     next();
 });
 app.put('/:subject/:id', function (req, res) {
-    // From the request obtain the path and update the corresponding array.
-    res.end('Got a PUT request at /physics/1');
-    console.log(req.param('subject'));
-    console.log(req.param('id'));
-    // I have trouble getting body value.
-    console.log(req.body);
+    update(METHOD.PUT, req, res);
 });
-
+// DELETE
 app.delete('*', function (req, res, next) {
     res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
     next();
 });
-app.delete('/physics/1', function (req, res) {
-    res.end('Got a DELETE request at /physics/1');
+app.delete('/:subject/:id', function (req, res) {
+    update(METHOD.DELETE, req, res);
+});
+
+// POST
+app.post('*', function (req, res, next) {
+    console.log("Reached post");
+    res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" });
+    next();
+});
+app.post('/:subject', function (req, res) {
+
+    //console.log(req.params.subject);
+    //console.log(req.body);
+
+    if (req.params.subject == 'physics') {
+        physics.push({id: req.body.id, fname: req.body.fname, lname: req.body.lname});
+        res.end (JSON.stringify(physics));
+    } else if (req.params.subject == 'chemistry') {
+        chemistry.push({id: req.body.id, fname: req.body.fname, lname: req.body.lname});
+        res.end (JSON.stringify(chemistry));
+    } else if (req.params.subject == 'biology') {
+        biology.push({id: req.body.id, fname: req.body.fname, lname: req.body.lname});
+        res.end (JSON.stringify(biology));
+    } else {
+
+    }
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
 
+
+/*
+TODO:
+
+. Deploy this server to Heroku
+. Persist using a postgres database
+. Get PUT, POST working
+. Improve code
+. Add error handling
+. Make this into a TS server (from a JS server)
+
+NOTES
+
+ //https://expressjs.com/en/starter/hello-world.html
+ //I have initialized the server to run from /users/schandrasekhar/WebstormProjects/practice-server1 as per instructions in above link
+ // Run node index.js after cd'ing to the above folder
+
+REFERENCES
+
+ //http://stackoverflow.com/questions/9177049/express-js-req-body-undefined
+ //https://expressjs.com/en/starter/hello-world.html
+
+ */
